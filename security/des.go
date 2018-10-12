a852ff8544aa42364fe2998c55f7b0756034bbb0
u35s <jintao7080@gmail.com>
2018-10-12 11:25:52 +0800

    删除多余代码

--- security/des.go
+++ security/des.go
@@ -2,18 +2,9 @@ package main
 
 import (
 	"crypto/cipher"
-	"crypto/des"
 )
 
 var desblock cipher.Block
-
-func init() {
-	var key [8]byte
-	for i := range key {
-		key[i] = byte(RandUint(0, 255))
-	}
-	desblock, _ = des.NewCipher(key[:])
-}
 
 func DesEncrypt(buf, encrypted []byte) {
 	desblock.Encrypt(buf, encrypted)
